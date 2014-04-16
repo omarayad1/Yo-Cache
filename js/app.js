5c8302a71a2ae4326227d3a221cf572bebef0c3e
omarayad1 <omarayad1@gmail.com>
2014-04-16 13:16:55 +0200

    created a handlebars template and an ember root route

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 (function() {
+  var fs;
 
+  fs = require('fs');
+
+  window.yo_cache = Ember.Application.create();
+
+  yo_cache.Router.map(function() {
+    return this.resource('start', {
+      path: '/'
+    });
+  });
 
 }).call(this);
